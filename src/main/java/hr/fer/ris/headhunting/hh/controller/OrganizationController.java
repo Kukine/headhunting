3ec3e752138407d6548b9d8afc1c7c1dda8e2772
LukaKusina <luka.kusina@fer.hr>
2020-05-17 22:31:45 +0200

    organization controller

---
+++ src/main/java/hr/fer/ris/headhunting/hh/controller/OrganizationController.java
@@ -0,0 +1,28 @@
+package hr.fer.ris.headhunting.hh.controller;
+
+import hr.fer.ris.headhunting.hh.data.entity.Organization;
+import hr.fer.ris.headhunting.hh.data.repository.OrganizationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/organization")
+public class OrganizationController {
+
+    private OrganizationRepository organizationRepository;
+
+    @Autowired
+    public OrganizationController(OrganizationRepository organizationRepository){
+        this.organizationRepository = organizationRepository;
+    }
+
+    @GetMapping("")
+    public List<Organization> getOrganizations(){
+        return this.organizationRepository.findAll();
+    }
+
+}
